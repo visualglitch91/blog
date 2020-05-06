8f9400f3013a2b447e6b8274bbd0bfb973eb7861
visualglitch91 <visualglitch91@protonmail.com>
2020-05-05 23:52:54 -0300

    Open external link in a new tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,13 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow",
+            },
+          },
         ],
       },
     },
